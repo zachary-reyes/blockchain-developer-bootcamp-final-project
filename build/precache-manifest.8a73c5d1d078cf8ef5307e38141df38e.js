96747c12dbdadf5418792b4e3f03cb8e6895079c
Zachary Reyes <zacharyaustin@outlook.com>
2021-11-29 10:56:12 -0800

    adds github pages deployment

---
+++ build/precache-manifest.8a73c5d1d078cf8ef5307e38141df38e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02612028e86f5d1dfe3b",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.cfaef1b5.chunk.css"
+  },
+  {
+    "revision": "02612028e86f5d1dfe3b",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.02612028.chunk.js"
+  },
+  {
+    "revision": "8cb13dbd9cc9aef07a09",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/1.fcb734f5.chunk.css"
+  },
+  {
+    "revision": "8cb13dbd9cc9aef07a09",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/1.8cb13dbd.chunk.js"
+  },
+  {
+    "revision": "2242dfc52fd5369ed931",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime~main.2242dfc5.js"
+  },
+  {
+    "revision": "9a4d792a5d69a4ac4d98342379ddcdf0",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  }
+];
