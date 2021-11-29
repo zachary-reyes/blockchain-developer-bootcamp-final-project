1ec972b2501b0f0b3a3a0e609dc086b8cd48f8aa
Zachary Reyes <zacharyaustin@outlook.com>
2021-11-29 11:23:27 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/blockchain-developer-bootcamp-final-project/precache-manifest.8a73c5d1d078cf8ef5307e38141df38e.js"
+  "/blockchain-developer-bootcamp-final-project/precache-manifest.f2c47d22c2fee81e8b8ecc9fa0fcf759.js"
 );
 
 workbox.clientsClaim();
